e12c20e70b08faa6ad3c24457b2e79ba952d1a84
Codeomii <omp83075@gmail.com>
2026-04-14 21:41:02 +0530

    14-04-2026

---
+++ Remove Spaces.py
@@ -0,0 +1,22 @@
+'''Remove Spaces
+
+Given a string s, remove all the spaces from the string and return the modified string.
+
+Examples:
+
+Input: s = "g eeks for ge eks"
+Output: "geeksforgeeks"
+Explanation: All space characters are removed from the given string while preserving the order of the remaining characters, resulting in the final string "geeksforgeeks".
+Input: s = "abc d "
+Output: "abcd"
+Explanation:  All space characters are removed from the given string while preserving the order of the remaining characters, resulting in the final string "abcd".
+Constraints:
+1 ≤ |s| ≤ 105
+
+'''
+
+# Solution:-- 
+
+class Solution:
+    def removeSpaces(self, s):
+        return s.replace(" ", "")
