81d854c87f0e709bc089eac47475cd4f6224f554
Codeomii <omp83075@gmail.com>
2026-04-15 20:35:53 +0530

    15-04-2026

---
+++ URLify a given string.py
@@ -0,0 +1,20 @@
+'''URLify a given string:--
+
+Given a string s, replace all the spaces in the string with '%20'.
+
+Examples:
+
+Input: s = "i love programming"
+Output: "i%20love%20programming"
+Explanation: The 2 spaces are replaced by '%20'
+Input: s = "Mr Benedict Cumberbatch"
+Output: "Mr%20Benedict%20Cumberbatch"
+Explanation: The 2 spaces are replaced by '%20'
+Constraints:
+1 ≤ s.size() ≤ 104'''
+
+# Solution:--
+
+class Solution:
+    def URLify(self, s: str) -> str:
+        return s.replace(" ", "%20")
