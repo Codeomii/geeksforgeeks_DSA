609f450cb17604001b46bf416a12eeac04b05e80
Codeomii <omp83075@gmail.com>
2026-04-27 20:26:54 +0530

    27-04-2026

--- Smallest window containing 0, 1 and 2.py
+++ Smallest window containing 0, 1 and 2.py
@@ -9,7 +9,8 @@ Examples :
 Input: s = "10212"
 Output: 3
 Explanation: The substring "102" is the shortest substring that contains all three characters '0', '1', and '2', so the answer is 3.
-Input: s = "12121"
+I
+nput: s = "12121"
 Output: -1
 Explanation: The character '0' is not present in the string, so no substring can contain all three characters '0', '1', and '2'. Hence, the answer is -1.
 Constraints:
